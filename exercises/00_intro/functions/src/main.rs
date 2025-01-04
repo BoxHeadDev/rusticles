ac6979a3d98886a26d59915059732037e7ae1c04
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-04 18:57:30 +0000

    solution/00_intro functions

--- exercises/00_intro/functions/src/main.rs
+++ exercises/00_intro/functions/src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     // Will the following functions compile?
     // What will be the output?
 
-    another_function();
-    print_labeled_measurement();
+    another_function(); // Output: Another Function
+    print_labeled_measurement(5, 'h'); // Arguments needed
     expressions();
     
     let x = plus_one(5);
@@ -12,23 +12,26 @@ fn main() {
     println!("{}", f({
         let y = 1;
         y + 1
-    }));
+    })); // Output: 3
 }
 
 fn another_function() {
     println!("Another function.");
 }
 
-fn print_labeled_measurement(value, unit_label) {
+// Parameter types
+fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 }
 
+// Statements don't return values
 fn expressions() {
-    let x = (let y = 6);
+    let x = let y = 6;
     println!("The value of x is: {x}");
 }
 
-fn plus_one(x: i32) {
+// Return type
+fn plus_one(x: i32) -> i32 {
     x + 1;
 }
 
