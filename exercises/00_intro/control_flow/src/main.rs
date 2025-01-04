28126c1b305853e327a3ba6b735bb3a2ca890e1b
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-04 20:31:59 +0000

    solution/00_intro control_flow

--- exercises/00_intro/control_flow/src/main.rs
+++ exercises/00_intro/control_flow/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
 fn single_condition(){
     let number = 3;
     
-    if number {
+    if number == 3 {
         println!("number was three");
     }
 }
@@ -23,21 +23,27 @@ fn single_condition(){
 fn multiple_conditions(){
     let number = 6;
     
-    println!("number is divisible by 4");
-    println!("number is divisible by 3");
-    println!("number is divisible by 2");
-    println!("number is not divisible by 4, 3, or 2");
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
 
+// Condition needs to return same type
 fn inline_expression() {
     let condition = true;
 
-    let number = if condition { 5 } else { "six" };
+    let number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {number}");
 }
 
-// Assign the value to result when counter is 10
+// Break out of loop
 fn loop_return_values() {
     let mut counter = 0;
 
@@ -45,17 +51,17 @@ fn loop_return_values() {
         counter += 1;
 
         if counter == 10 {
-            counter * 2;
+            break counter * 2;
         }
     };
 
     println!("The result is {result}");
 }
 
-// Update to break out of all loops when count is 2
+// Use label to break out of inner loop
 fn inner_loop_break() {
     let mut count = 0;
-    loop {
+    'counting_up: loop {
         println!("count = {count}");
         let mut remaining = 10;
 
@@ -65,7 +71,7 @@ fn inner_loop_break() {
                 break;
             }
             if count == 2 {
-                break;
+                break 'counting_up;
             }
             remaining -= 1;
         }
@@ -75,11 +81,14 @@ fn inner_loop_break() {
     println!("End count = {count}");
 }
 
-// Implement a while loop that counts down from 3
 fn while_loop() {
     let mut number = 3;
 
-    println!("{number}!");
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
 
     println!("LIFTOFF!!!");
 }
@@ -89,19 +98,26 @@ fn collection_while_loop() {
     let a = [10, 20, 30, 40, 50];
     let mut index = 0;
 
-    println!("the value is: {}", a[index]);
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
 }
 
-// Loop through the collection using a for loop
 fn collection_for_loop() {
     let a = [10, 20, 30, 40, 50];
 
-    println!("the value is: {}");
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
 
-// Loop from 4 to 1 using a range
 fn range_loop() {
-    println!("{number}!");
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
     
     println!("LIFTOFF!!!");
 }
