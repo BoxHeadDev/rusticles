e206287f3b4b732b6e93ad41e876265db2c324b5
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-05 19:02:58 +0000

    solution/01_ownership borrowing

--- exercises/01_ownership/borrowing/src/main.rs
+++ exercises/01_ownership/borrowing/src/main.rs
@@ -11,40 +11,45 @@ fn main() {
     lifetime_1();
 }
 
+// Pointers
 fn borrowing_1() {
     let m1 = String::from("Hello");
     let m2 = String::from("world");
-    greet(m1, m2);
+    greet(&m1, &m2);
     let s = format!("{} {}", m1, m2);
 }
 
-fn greet(g1: String, g2: String) {
+fn greet(g1: &String, g2: &String) {
     println!("{} {}!", g1, g2);
 }
 
 fn borrowing_2() {
     let mut x: Box<i32> = Box::new(1);
-    let a: i32 = x;
-    x += 1;
+    let a: i32 = *x; // *x reads the heap value, so a = 1
+    *x += 1; // *x on the left-side modifies the heap value,
+             //     so x points to the value 2
 
-    let r1: &Box<i32> = x;
-    let b: i32 = r1;
+    let r1: &Box<i32> = &x; // r1 points to x on the stack
+    let b: i32 = **r1; // two dereferences get us to the heap value
 
-    let r2: &i32 = x;
-    let c: i32 = r2;
+    let r2: &i32 = &*x; // r2 points to the heap value directly
+    let c: i32 = *r2; // so only one dereference is needed to read it
 }
 
+// Implicit referencing
 fn borrowing_3() {
     let x: Box<i32> = Box::new(-1);
-    let x_abs = i32::abs(*x);
+    let x_abs = x.abs();
 
     let r: &Box<i32> = &x;
-    let r_abs = i32::abs(**r);
+    let r_abs = r.abs();
 
     let s = String::from("Hello");
-    let s_len = str::len(&s);
+    let s_len = s.len();
 }
 
+// v is aliased and mutated
+// Push invalidates the reference
 fn borrowing_4() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &i32 = &v[2];
@@ -52,6 +57,7 @@ fn borrowing_4() {
     println!("Third element is {}", *num);
 }
 
+// Mutable references
 fn borrowing_5() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &mut i32 = &mut v[2];
@@ -60,16 +66,17 @@ fn borrowing_5() {
     println!("Vector is now {:?}", v);
 }
 
+// Downgrade mutable reference
 fn borrowing_6() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let num: &mut i32 = &mut v[2];
-    let num2: &i32 = &*num;
+    let num2: &i32 = &*num; // read only
     println!("{} {}", *num, *num2);
 }
 
 fn lifetime_1() {
     let mut x = 1;
-    let y = &x;
-    let z = *y;
+    let y = &x; // Start
+    let z = *y; // Stop
     x += z;
 }
