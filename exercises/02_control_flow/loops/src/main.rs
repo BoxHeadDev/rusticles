16780c694381eea563911e1ccc3f054e3f937132
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-07 18:19:18 +0000

    updated folder structure

--- exercises/02_control_flow/loops/src/main.rs
+++ exercises/02_control_flow/loops/src/main.rs
@@ -1,46 +1,9 @@
 fn main() {
-    // Will the following compile?
-    // What is the output?
-    single_condition();
-    multiple_conditions();
-    inline_expression();
-
     loop_return_values();
     inner_loop_break();
     while_loop();
     collection_while_loop();
     collection_for_loop();
-}
-
-fn single_condition(){
-    let number = 3;
-    
-    if number == 3 {
-        println!("number was three");
-    }
-}
-
-fn multiple_conditions(){
-    let number = 6;
-    
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
-}
-
-// Condition needs to return same type
-fn inline_expression() {
-    let condition = true;
-
-    let number = if condition { 5 } else { 6 };
-
-    println!("The value of number is: {number}");
 }
 
 // Break out of loop
@@ -114,10 +77,9 @@ fn collection_for_loop() {
 }
 
 fn range_loop() {
-
     for number in (1..4).rev() {
         println!("{number}!");
     }
-    
+
     println!("LIFTOFF!!!");
 }
