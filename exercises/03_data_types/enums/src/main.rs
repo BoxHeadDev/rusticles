f6a779b2003fb83c5cb880b3f7ea4b7d9cb84315
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-12 14:12:27 +0000

    solution/03_data_types enums

--- exercises/03_data_types/enums/src/main.rs
+++ exercises/03_data_types/enums/src/main.rs
@@ -1,5 +1,9 @@
 // Create enum for different kinds of IP addresses (V4, V6)
 // IpAddrKind
+enum IpAddrKind {
+    V4,
+    V6,
+}
 
 fn main() {
     enum_1();
@@ -13,26 +17,32 @@ fn main() {
 
 // Create instances of each enum variant
 fn enum_1() {
-    // let four = ??
-    // let six = ??
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 }
 
-// Implement instances of IpAddr using the IpAddrKind enum
+// Implement instances of IpAddr usingthe IpAddrKind enum
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
 }
 fn enum_2() {
-    // let home = ??
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
 
-    // let loopback = ??
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
 
 // Instead of using the enum with a struct
 // put the data directly into each enum variant
 enum IpAddr {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 fn enum_3() {
@@ -43,7 +53,7 @@ fn enum_3() {
 
 // Store V4 addresses as four u8 values but still express V6 addresses as one String value
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
@@ -54,32 +64,37 @@ fn enum_4() {
 }
 
 // Convert the following structs into a single enum Message
-struct QuitMessage; // unit struct
-struct MoveMessage {
-    x: i32,
-    y: i32,
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
-struct WriteMessage(String); // tuple struct
-struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
+}
 
 // Create call method for message to replace WriteMessage
 fn enum_5() {
-    let m = WriteMessage(String::from("hello"));
+    let m = Message::Write(String::from("hello"));
     m.call();
 }
 
 // Add types to the following variables
 fn enum_6() {
-    let some_number = Some(5);
-    let some_char = Some('e');
+    let some_number: Option<i8> = Some(5);
+    let some_char: Option<i8> = Some('e');
 
-    let absent_number = None;
+    let absent_number: Option<i32> = None;
 }
 
-// Fix the following error
+// Rust cannot add two different types i8 and Option<i8>
 fn enum_7() {
     let x: i8 = 5;
-    let y: Option<i8> = Some(5);
+    let y: i8 = Some(5); // Remove Option type
 
     let sum = x + y;
 }
