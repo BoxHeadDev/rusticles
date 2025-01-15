274faa4df1d5d0da270cc315bfb4960628b79d41
Chris Wren <chriswrendeveloper@gmail.com>
2025-01-15 14:15:14 +0000

    solution/03_data_types vectors

--- exercises/03_data_types/vectors/src/main.rs
+++ exercises/03_data_types/vectors/src/main.rs
@@ -17,20 +17,25 @@ fn main() {
 
 fn vectors_1() {
     // Create an empty vector of integers
+    let mut v1: Vec<i32> = Vec::new();
 
-    // Add integers to the vector (eg. 5,6,7,8)
+    // Add integers to the vector
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
 }
 
 fn vectors_2() {
     // Create a populated vector of integers
-    let v2 = ??;
+    let v2 = vec![1, 2, 3];
 
     // Assign the third element in the vector (index)
-    let third= ??;
+    let third: &i32 = &v[2];
     println!("The third element is {third}");
 
     // Assign the third element in the vector (method)
-    // let third = ??;
+    let third: Option<&i32> = v.get(2);
     match third {
         Some(third) => println!("The third element is {third}"),
         None => println!("There is no third element."),
@@ -40,12 +45,13 @@ fn vectors_2() {
 fn vectors_3() {
     // What do each of the following return below?
     let v = vec![1, 2, 3, 4, 5];
-    let does_not_exist = &v[100];
-    let does_not_exist = v.get(100);
+    let does_not_exist = &v[100]; // panic
+    let does_not_exist = v.get(100); // none
 }
 
 fn vectors_4() {
     // What is wrong with the following?
+    // can’t have mutable and immutable references in the same scope
     let mut v = vec![1, 2, 3, 4, 5];
     let first = &v[0];
     v.push(6);
@@ -55,19 +61,23 @@ fn vectors_4() {
 fn vectors_5() {
     // Loop over v and print each element after adding 1
     let v = vec![100, 32, 57];
-
-    let n_plus_one: i32 = *n_ref + 1;
-    println!("{n_plus_one}");
+    for n_ref in &v {
+        let n_plus_one: i32 = *n_ref + 1; // n_ref has type &i32
+        println!("{n_plus_one}");
+    }
 }
 
 fn vectors_6() {
     // Loop over v and add 50 to each element
     let mut v = vec![100, 32, 57];
-
+    for n_ref in &mut v {
+        *n_ref += 50; // n_ref has type &mut i32
+    }
 }
 
 fn vectors_7() {
     // What is wrong with the following?
+    // Non-copyable types cannot be moved out of a vector by indexing
     let v = vec![String::from("Hello ")];
     let mut s = v[0];
     s.push_str("world");
@@ -76,11 +86,23 @@ fn vectors_7() {
 
 fn vectors_8() {
     // Fix the following? (Vectors can only have one type)
-    let row = vec![3),String::from("blue"),10.12];
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
 }
 
 fn vectors_9() {
     // Does the following compile?
+    // No, only i can be mutated not v
     let mut v = vec![1, 2, 3];
     for i in &mut v {
         v.push(*i);
@@ -93,33 +115,34 @@ fn vectors_10() {
     let mut v: Vec<i32> = vec![1, 2, 3];
     let mut v2: Vec<&mut i32> = Vec::new();
     for i in &mut v {
-        v2.push(i);
+        v2.push(i); // Pass pointer not value
     }
-    *v2[0] = 5;
+    *v2[0] = 5; // Updates both due to pointer
     let a = *v2[0];
     let b = v[0];
-    println!("{a} {b}");
+    println!("{a} {b}"); // 5 5
 }
 
 fn iterator_1() {
     // What are the values of the following Iterator?
     let mut v: Vec<i32> = vec![1, 2];
     let mut iter: Iter<'_, i32> = v.iter();
-    let n1: &i32 = iter.next().unwrap();
-    let n2: &i32 = iter.next().unwrap();
-    let end: Option<&i32> = iter.next();
+    let n1: &i32 = iter.next().unwrap(); // 1
+    let n2: &i32 = iter.next().unwrap(); // 2
+    let end: Option<&i32> = iter.next(); // none
 }
 
 fn iterator_2() {
     // Iterate over a vector without using a pointer
     let mut v: Vec<i32> = vec![1, 2];
-    let mut iter: ? = ??;
+    let mut iter: Range<usize> = 0..v.len(); // range of indexes
     let i1: usize = iter.next().unwrap();
     let n1: &i32 = &v[i1];
 }
 
 fn iterator_3() {
     // Why does the following not compile?
+    // v.iter() removes the W permission from *v
     fn dup_in_place(v: &mut Vec<i32>) {
         for n_ref in v.iter() {
             v.push(*n_ref);
